be34395dfab3421d07736d81b7b2ec768342232e
Manishkhatiwada <manish.khatiwada150@gmail.com>
2016-12-05 10:52:05 +0545

    Add files via upload

---
+++ manish.prgrmingb slc.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a=0;
+    printf("enter the first number/n");
+    scanf("%d",&a);
+    if(a>=80&&a<100)
+    {
+        printf("you are passed in distinction");
+    }
+    if(a>=60 &&a<70)
+    {
+        printf("you are passed in first division");
+    }
+    if(a>=50&&a<60)
+    {
+        printf("you are passed in second division");
+    }
+    if(a>=40&&a<50)
+    {
+        printf("you are passed in third division");
+    }
+    getch();
+    return(0);
+}
